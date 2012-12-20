30e7324ba3224436e2d2634b9c9694ec149b8fe8
sipay <sipayrt@ya.ru>
2012-12-20 22:51:56 +0200

    popup slider

--- js/custom.js
+++ js/custom.js
@@ -73,46 +73,35 @@ $(document).ready(function() {
 		
 		slideCount = $('.portfolio-thumbs').children().length;
 		slideIndex = $('[data-js="popup-open"]').index(this);
-		allImagesContainer = [];
+		allImagesArray = [];
 		$('[data-js="popup-open"]').each(function(){
-			//allImagesContainer.push(this);
-			allImagesContainer[allImagesContainer.length] = $(this);
+			allImagesArray.push($(this).html());
 		});
-		bf = allImagesContainer.toString();
-		alert(bf)
+		allImagesContainer = allImagesArray.join('');
 		
-		return false;
+
 		nextSlide = slideIndex + 1;
 		prevSlide = ((slideIndex - 1) <= 0) ? (slideCount - 1) : slideIndex - 1;
 		stopSlide = slideCount - 1;
-/*		popupSlider = '<div class="popup-content-wrap">
-							<div class="popup-content">
-								<div class="close-popup-button"></div>
-								<div class="popup-image-wrap">' + $('[data-js="image-slider"] .field-items').html() + '</div>
-								<div class="popup-buttons">
-									<div class="popup-button-item prev"></div>
-									<div class="popup-button-item next"></div>
-								</div>
-							</div>
-						</div>';*/
-		$('body').prepend('<div class="popup-bg"></div>');
+		popupSlider = '<div class="popup-bg"></div><div class="popup-content-wrap"><div class="popup-content"><div class="close-popup-button"></div><div class="popup-image-wrap">' + allImagesContainer + '</div><div class="popup-buttons"><div class="popup-button-item prev"></div><div class="popup-button-item next"></div></div></div></div>';
 		$('body').prepend(popupSlider);
-		$('.popup-image-wrap .field-item-'+slideIndex).animate({'z-index': '1', 'opacity': '1'}, 500);
-		return false;
+		$('.slide-'+slideIndex).animate({'z-index': '1', 'opacity': '1'}, 500);
+
 			
 		// next slide
 		$('.popup-button-item.next').live('click', function(event){
+
 			if(!event.isPropagationStopped()){
-				$('.popup-image-wrap .field-item').each(function(){
+				$('.popup-image-wrap .portfolio-thumbs-item-image').each(function(){
 					$(this).animate({'z-index': '0', 'opacity': '0'}, 500);
 				});
 				if(nextSlide > stopSlide){
-					$('.popup-image-wrap .field-item-0').animate({'z-index': '1', 'opacity': '1'});
+					$('.popup-image-wrap .slide-0').animate({'z-index': '1', 'opacity': '1'});
 					nextSlide = 1;
 					prevSlide = stopSlide;
 					
 				}else{
-					$('.popup-image-wrap .field-item-' + nextSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
+					$('.popup-image-wrap .slide-' + nextSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
 					nextSlide ++;
 					if(prevSlide == stopSlide){
 						prevSlide = 0;
@@ -128,11 +117,11 @@ $(document).ready(function() {
 		$('.popup-button-item.prev').live('click', function(event){
 			if(!event.isPropagationStopped()){
 				prevHide = (prevSlide >= stopSlide) ? 0 : (prevSlide + 1);
-				$('.popup-image-wrap .field-item').each(function(){
+				$('.popup-image-wrap .portfolio-thumbs-item-image').each(function(){
 					$(this).animate({'z-index': '0', 'opacity': '0'}, 500);
 				});
 				if(prevSlide >= 0){
-					$('.popup-image-wrap .field-item-' + prevSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
+					$('.popup-image-wrap .slide-' + prevSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
 					if(nextSlide <= 0){
 						nextSlide = stopSlide;
 					}else{
@@ -140,7 +129,7 @@ $(document).ready(function() {
 					}
 					prevSlide --;
 				}else{
-					$('.popup-image-wrap .field-item-' + stopSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
+					$('.popup-image-wrap .slide-' + stopSlide).animate({'z-index': '1', 'opacity': '1'}, 500);
 					nextSlide = 0;
 					prevSlide = stopSlide - 1;
 				}
@@ -158,6 +147,11 @@ $(document).ready(function() {
 		
 		
 		
+	});
+	
+	$('.close-popup-button, .popup-bg').live('click',function(){
+		$('.popup-content-wrap, .popup-bg').remove();
+
 	});
 	/* end popup */
 	
